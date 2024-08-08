94f7edfe9004a38ba897505842ec80e6609b9771
Camilo-fd <156461877+Camilo-fd@users.noreply.github.com>
2024-08-08 17:46:03 -0500

    feat: :sparkles: Añadida ruta para cancelar asientos

--- server/router/boleto.routes.js
+++ server/router/boleto.routes.js
@@ -16,4 +16,10 @@ appBoleto.post("/reserveSeats", async (req, res) => {
     res.status(201).json(resultado);
 });
 
+appBoleto.get("/cancelSeats/:boleto_id", async(req, res) => {
+    let objBoleto = new boleto();
+    const idObject = { boleto_id: parseInt(req.params.boleto_id) };
+    res.status(200).send(await objBoleto.cancelSeatReservation(idObject))
+})
+
 module.exports = appBoleto;
