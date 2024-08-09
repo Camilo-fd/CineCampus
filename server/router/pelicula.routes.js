8d0715bb7a3efa927356cc53d4f8a8c52db53f61
Camilo-fd <156461877+Camilo-fd@users.noreply.github.com>
2024-08-08 22:09:13 -0500

    refactor: :sparkles: Modificacion de codigo para validar las consultas en pelicula

--- server/router/pelicula.routes.js
+++ server/router/pelicula.routes.js
@@ -1,16 +1,26 @@
 const express = require("express")
 const appPelicula = express.Router()
+const { query, validationResult } = require("express-validator");
 const pelicula = require("../model/pelicula")
 
-appPelicula.get("/all", async(req, res) => {
-    let objPeliculas = new pelicula 
-    res.status(200).send(await objPeliculas.getAllMovis())
+appPelicula.get("/id", [query("id").notEmpty()], async(req, res) => {
+    const error = validationResult(req)
+    if (!error.isEmpty()) {
+        return res.status(400).json({ errors: error.array() });
+    } else {
+        let objPeliculas = new pelicula 
+        const idObject = { id: parseInt(req.query.id) };
+        res.status(200).send(await objPeliculas.getMovisId(idObject))
+    }   
 })
 
-appPelicula.get("/id/:id", async(req, res) => {
-    let objPeliculas = new pelicula 
-    const idObject = { id: parseInt(req.params.id) };
-    res.status(200).send(await objPeliculas.getMovisId(idObject))
+appPelicula.get("/all", async(req, res) => {
+    if (Object.keys(req.query).length) { 
+        return res.status(400).json({ message: "No es necesario una query" })
+    } else { 
+        let objPeliculas = new pelicula 
+        res.status(200).send(await objPeliculas.getAllMovis())
+    }
 })
 
 module.exports = appPelicula;
