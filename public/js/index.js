f0acda0dc7a74f006ad8ec4933acd95ae3243589
Camilo-fd <156461877+Camilo-fd@users.noreply.github.com>
2024-08-14 22:46:25 -0500

    feat: :sparkles: Extraccion de datos completado, y creacion de codigo para buscar pelicula

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,11 @@
 import { loadMovies, MoviesComingSoon } from './module/pelicula.js';
+import { search } from './module/search.js';
 
 async function run() {
     try {
         await loadMovies();
         await MoviesComingSoon();
+        await search();
     } catch (error) {
         console.log(error);
     }
